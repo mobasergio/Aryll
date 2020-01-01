3fa7eb8ea3458b161bc39965bc3ff225ea556626
mobasergio <mobasergio@gmail.com>
2020-01-01 21:12:06 +0100

    Add Google Analytics

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactGA from 'react-ga';
 import './styles/style.css'
 import { Router } from "@reach/router"
 import Navbar from './components/Navbar'
@@ -9,6 +10,8 @@ import Emotes from './pages/Emotes'
 import Concept from './pages/Concept'
 import Contact from './pages/Contact'
 import Disclaimer from './pages/Disclaimer'
+
+ReactGA.initialize('UA-112238639-2');
 
 export default () => (
   <div className="container" onContextMenu={(e)=> e.preventDefault()}>
