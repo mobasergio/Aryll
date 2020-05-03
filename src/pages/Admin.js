a3e96ac492e27884d06e614efbc0707d055abee0
mobasergio <mobasergio@gmail.com>
2020-05-03 20:58:06 +0200

    Add error codes

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -5,16 +5,26 @@ import Panel from "./Panel";
 const Admin = () => {
   const [account, setAccount] = useState({email: "", password: ""});
   const [panel, setPanel] = useState(false);
-  const [error, setError] = useState(false);
+  const [error, setError] = useState(0);
+
+  const errorCodes = {
+    0: "",
+    1: "Asi no es, ponlo bien",
+    2: "Que asi no es miralo bien que esta mal",
+    3: "Mira yo paso ya"
+  }
+  
+  const errorMessages = () => {
+    if (error != 3) { setError((prevState) => (prevState + 1)) }
+  }
 
   const handleSubmit = event => {
     event.preventDefault();
     firebase.auth().signInWithEmailAndPassword(account.email, account.password).then((account) => {
       setPanel(true)
-      console.log(account)
     })
     .catch(function (e) {
-      setError(true);
+      errorMessages();
       setAccount({email: "", password: ""})
     });
   }
@@ -35,10 +45,10 @@ const Admin = () => {
       <div id="admin">
         <form onSubmit={handleSubmit}>
           <h1>Admin panel</h1>
-          <input name="email" type="email" placeholder="Email" value={account.email} onChange={handleInputChange}/>
-          <input name="password" type="password" placeholder="Password" value={account.password} onChange={handleInputChange}/>
+          <input name="email" type="email" placeholder="Email" value={account.email} onChange={handleInputChange} required/>
+          <input name="password" type="password" placeholder="Password" value={account.password} onChange={handleInputChange} required/>
           <input type="submit" value="Log in"/>
-          <p style={{display: error ? 'block' : 'none'}}>Ya lo has escrito mal miralo bien por favor ya</p>
+          <p style={{display: error != 0 ? 'block' : 'none'}}>{errorCodes[error]}</p>
         </form>
       </div>
     </main>
